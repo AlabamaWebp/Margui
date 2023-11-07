27f5735a65e7cec98f5f57420084d8ff0f27a82d
AlabamaWebp <dimyrp@yandex.ru>
2023-11-07 09:43:52 +0300

    styles hoverContainder

--- src/components/hover-container/hoverContainer.js
+++ src/components/hover-container/hoverContainer.js
@@ -8,11 +8,11 @@ const HoverContainer = (vars) => {
         <div className='hovverTemplate'>
             <div className="hoverContainer">
                 <span className='htext'>{vars.text}</span>
-            </div>
-            <div className="hoverMenu">
-                {vars.items.map((item, index) => (
-                    <span key={index} className='hoverMenuItem' onClick={change}>{item}</span>
-                ))}
+                <div className="hoverMenu">
+                    {vars.items.map((item, index) => (
+                        <span key={index} className='hoverMenuItem' onClick={change}>{item}</span>
+                    ))}
+                </div>
             </div>
         </div>
     )

ecc9b4728c111955bc5964b51b99e3c8e3a3e6fd
AlabamaWebp <dimyrp@yandex.ru>
2023-12-22 13:52:57 +0300

    для qr кодов

--- src/data/header/cabs.js
+++ src/data/header/cabs.js
@@ -5,11 +5,11 @@ import c217 from '../pannellum/217/c217';
 import c204 from '../pannellum/204/c204';
 import c208 from '../pannellum/208/c208';
 
-const cabs1 = [["217 каб.", c217, "?cab=217"],
-               ["208 каб.", c208, "?cab=208"],
-               ["114 каб.", c114, "?cab=114"],
-               ["204 каб.", c204, "?cab=204"],
-               ["116 каб.", c116, "?cab=116"],
+const cabs1 = [["217 каб.", c217, "?cab=C217"],
+               ["208 каб.", c208, "?cab=C208"],
+               ["114 каб.", c114, "?cab=C114"],
+               ["204 каб.", c204, "?cab=C204"],
+               ["116 каб.", c116, "?cab=C116"],
                ["Музей", Museum_data, "?cab=mus"],];
 
 export default cabs1;
